0ab6268e84b9fc09fe51b3a449ea1583340dafd3
a-kanteruk <a.kanteruk@tg.dunice.net>
2023-03-27 10:38:56 +0300

    First task is done

---
+++ src/main/java/net/dunice/Animal.java
@@ -0,0 +1,52 @@
+package net.dunice;
+
+import javax.print.DocFlavor;
+
+public class Animal{
+    String name;
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Spike", 40);
+        Cat cat = new Cat("Luci", 6);
+        Animal cat2 = new Cat("Lindy", 3);
+
+        cat.meow();
+        dog.bark();
+
+        dog.showInfo();
+        cat.showInfo();
+
+        cat2.showInfo();
+        }
+
+    public void showInfo(){
+        System.out.println("Меня зовут " + name);
+    }
+}
+
+class Dog extends Animal{
+    int weight;
+
+    public Dog(String someName, int someWeight){
+        this.name = someName;
+        this.weight = someWeight;
+    }
+
+    public void bark(){
+        System.out.println("Гаф! Гаф!");
+    }
+}
+
+class Cat extends Animal{
+    int weight;
+    public Cat(String someName, int someWeight){
+        this.name = someName;
+        this.weight = someWeight;
+    }
+
+    public void meow(){
+        System.out.println("Мяу, Мяу!");
+    }
+}
+
+
